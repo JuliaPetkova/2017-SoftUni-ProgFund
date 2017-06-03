613a200ef853700f2d56c1250a36c05ca61818a3
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 23:19:21 +0300

    add task 4-2-18 Different Integer Size

---
+++ 04-DataTypeAndVariables/4-2-18-DifferentIntegerSize/4-2-18-DifferentIntegerSize.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_18_DifferentIntegerSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // sbyte < byte < short < ushort < int < uint < long
+
+            var inputNum = Console.ReadLine();
+
+            bool isInRange = false;
+            string dataType = "";
+
+            try
+            {
+                sbyte sbyteNum = sbyte.Parse(inputNum);
+                dataType += "* sbyte\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                byte byteNum = byte.Parse(inputNum);
+                dataType += "* byte\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                short shortNum = short.Parse(inputNum);
+                dataType += "* short\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                ushort ushortNum = ushort.Parse(inputNum);
+                dataType += "* ushort\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                int intNum = int.Parse(inputNum);
+                dataType += "* int\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                uint uintNum = uint.Parse(inputNum);
+                dataType += "* uint\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            try
+            {
+                long longNum = long.Parse(inputNum);
+                dataType += "* long\n";
+                isInRange = true;
+            }
+            catch (Exception)
+            {
+            }
+
+            if (isInRange)
+            {
+                Console.WriteLine("{0} can fit in:", inputNum);
+                Console.WriteLine(dataType);
+            }
+            else
+            {
+                Console.WriteLine("{0} can't fit in any type", inputNum);
+            }
+        }
+    }
+}
---
+++ 04-DataTypeAndVariables/4-2-19-TheaThePhotographer/4-2-19-TheaThePhotographer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_19_TheaThePhotographer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
