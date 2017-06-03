dcca825800a23b118c1f56fd941e692bb5e1c14a
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 11:05:29 +0300

    adding new task: topic 4.2, task 7

---
+++ 04-DataTypeAndVariables/4-2-07-ExchangeVariables/4-2-07-ExchangeVariables.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_07_ExchangeVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Before: \na = {a}\nb = {b}");
+
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"After: \na = {a}\nb = {b}");
+
+
+        }
+    }
+}
