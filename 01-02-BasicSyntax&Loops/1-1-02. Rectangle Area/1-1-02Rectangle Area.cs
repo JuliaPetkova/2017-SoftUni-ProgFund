489887c7cfbc9d0f6f27de7b30a4a35df465317a
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 06:48:42 +0300

    renamed files

---
+++ 01-02-BasicSyntax&Loops/1-1-02. Rectangle Area/1-1-02Rectangle Area.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task02_RectangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+
+            double s = a * b;
+            Console.WriteLine($"{s:F2}");
+
+        }
+    }
+}
---
+++ 01-02-BasicSyntax&Loops/1-1-03. Miles to Kilometers/1-1-03Miles to Kilometers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task04_BeverageLabels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double volume = double.Parse(Console.ReadLine());
+            double energy = double.Parse(Console.ReadLine());
+            double sugar = double.Parse(Console.ReadLine());
+
+            double kcal = volume * energy / 100;
+            double Sugar = volume * sugar / 100;
+
+            Console.WriteLine($"{volume}ml {name}:");
+            Console.WriteLine($"{kcal}kcal, {Sugar}g sugars");
+            //660kcal, 154g sugars
+
+        }
+    }
+}
