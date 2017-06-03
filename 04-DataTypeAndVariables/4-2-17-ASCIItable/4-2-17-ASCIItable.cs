dedb7804b2742b8b3d092e5fe22008abc7788417
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 21:56:01 +0300

    add 4-2-17 ASCII table

---
+++ 04-DataTypeAndVariables/4-2-17-ASCIItable/4-2-17-ASCIItable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_17_ASCIItable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int char1 = int.Parse(Console.ReadLine());
+            int char2 = int.Parse(Console.ReadLine());
+
+            for (char i = (char)char1; i <= (char)char2; i++)
+            {
+                Console.Write($"{i} ");
+            }
+        }
+    }
+}
