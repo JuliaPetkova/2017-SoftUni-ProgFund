791e850d6bc51b297267edfa4f32d092c4984c98
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 18:50:22 +0300

    add task 4-2-13-VowelOrDigit

---
+++ 04-DataTypeAndVariables/4-2-13-VoweOrDigit/4-2-13-VowelOrDigit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_13_VoweOrDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+       
+                string symbol = Console.ReadLine();
+
+                bool isDig = ((symbol == "0") || (symbol == "1") || (symbol == "2") || (symbol == "3") || (symbol == "4") || (symbol == "5") || (symbol == "6") || (symbol == "7") || (symbol == "8") || (symbol == "9"));
+                bool isVowel = (symbol == "а") || (symbol == "e") || (symbol == "i") || (symbol == "o") || (symbol == "u");
+                if (isVowel == true)
+                {
+                    Console.WriteLine("vowel");
+                }
+                else if (isDig == true)
+                {
+                    Console.WriteLine("digit");
+                }
+                else
+                {
+                    Console.WriteLine("other");
+                }
+        }
+    }
+}
