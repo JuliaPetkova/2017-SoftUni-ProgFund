8eb56972253b6c09119998317545a36ce22d6c9a
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 21:19:43 +0300

    add 4-2-15-RefactorFastPrimeChecker

---
+++ 04-DataTypeAndVariables/4-2-15-RefactorFastPrimeChecker/4-2-15-RefactorFastPrimeChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_15_RefactorFastPrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int num = 2; num <= n; num++)
+            {
+                bool isPrime = true; // Prime- дели се само на себе си и на 1
+                for (int divider = 2; divider <= num / 2; divider++)
+                {
+                    if (num % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine($"{num} -> {isPrime}");
+
+            }
+        }
+    }
+}
