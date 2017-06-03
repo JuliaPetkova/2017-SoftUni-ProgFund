fe6becd665d835a9e231c276015484562b46c5ed
JuliaPetkova <julia.petkova@gmail.com>
2017-06-04 00:41:26 +0300

    add task 4-2-19 Thea The Photographer

--- 04-DataTypeAndVariables/4-2-19-TheaThePhotographer/4-2-19-TheaThePhotographer.cs
+++ 04-DataTypeAndVariables/4-2-19-TheaThePhotographer/4-2-19-TheaThePhotographer.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace _4_2_19_TheaThePhotographer
@@ -10,7 +11,17 @@ namespace _4_2_19_TheaThePhotographer
     {
         static void Main(string[] args)
         {
+            double allPics = double.Parse(Console.ReadLine());
+            double filterTime = double.Parse(Console.ReadLine());
+            double goodPics = double.Parse(Console.ReadLine());
 
+            goodPics = Math.Ceiling((allPics * goodPics) / 100.0);
+            double uploadTime = double.Parse(Console.ReadLine());
+
+            double totalTime = (allPics * filterTime) + (goodPics * uploadTime);
+            TimeSpan newTime = TimeSpan.FromSeconds((long)totalTime);
+
+            Console.WriteLine("{0:d1}:{1:d2}:{2:d2}:{3:d2}", newTime.Days, newTime.Hours, newTime.Minutes, newTime.Seconds);
         }
     }
 }
