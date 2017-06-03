77fcab4f60b2fea751525ef23642889d798019ab
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 19:35:38 +0300

    add task 4-2-14- Integer to Hex and to Binary

---
+++ 04-DataTypeAndVariables/4-2-14-IntegerHexBinary/4-2-14-IntegerHexBinary.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_14_IntegerHexBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int decValue = int.Parse(Console.ReadLine());
+
+            //string hexValue = string.Format("{0:x}", decValue); вариант с форматиране
+          
+            string binary = Convert.ToString(decValue, 2); // 2- binary
+            string hexValue = Convert.ToString(decValue, 16).ToUpper(); // 16-hexadecimal
+            
+            Console.WriteLine(hexValue);
+            Console.WriteLine(binary);
+
+        }
+    }
+}
