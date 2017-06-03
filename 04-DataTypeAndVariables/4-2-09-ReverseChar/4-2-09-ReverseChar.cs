f8eef3adb13e5010c7023542cc00a28ef3cebea8
JuliaPetkova <julia.petkova@gmail.com>
2017-06-03 14:39:07 +0300

    new tasks:4-2-09 Reverse char, 4-2-10 Centuries, 4-2-11 Speed Units convertion

---
+++ 04-DataTypeAndVariables/4-2-09-ReverseChar/4-2-09-ReverseChar.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_09_ReverseChar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+
+            char x = a;
+            a = c;
+            c = x;
+            Console.WriteLine($"{a}{b}{c}");
+
+        }
+    }
+}
---
+++ 04-DataTypeAndVariables/4-2-10 Centuries/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_2_10_Centuries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int century = int.Parse(Console.ReadLine());
+
+            int years = century * 100;
+            int days = (int)(years * 365.2422);
+            int hours = days * 24;
+            long minutes = hours * 60;
+            long seconds = minutes * 60;
+            long milliseconds = seconds * 1000;
+            long microseconds = milliseconds * 1000;
+            //decimal nanoseconds = 1000000 * microseconds;
+
+            Console.WriteLine
+                ($"{century} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes = {seconds} seconds = {milliseconds} milliseconds = {microseconds} microseconds = {microseconds}000 nanoseconds");
+        }
+    }
+}
