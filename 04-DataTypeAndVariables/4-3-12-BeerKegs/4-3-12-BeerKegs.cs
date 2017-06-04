9df75b3b902513cda3025c1b229f2901382d888b
JuliaPetkova <julia.petkova@gmail.com>
2017-06-04 16:32:05 +0300

    add task 4-3-12 Biggest keg

---
+++ 04-DataTypeAndVariables/4-3-12-BeerKegs/4-3-12-BeerKegs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_12_BeerKegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double volume = 1;
+            double tempVolume = 1;
+
+            string nameBiggestKeg = string.Empty;
+            string nameKeg = string.Empty;
+
+
+            for (int i = 0; i < n; i++)
+            {
+                nameKeg = Console.ReadLine();
+                double r = double.Parse(Console.ReadLine());
+                int heigh = int.Parse(Console.ReadLine());
+
+                tempVolume = Math.PI * r * r * heigh;
+
+                if (tempVolume > volume)
+                {
+                    volume = tempVolume;
+                    nameBiggestKeg = nameKeg;
+                }
+            }
+            Console.WriteLine(nameBiggestKeg);
+        }
+    }
+}
