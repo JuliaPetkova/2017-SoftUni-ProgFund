17acdaee2d28ce8e97be642dcc4fc1689e6efa42
JuliaPetkova <julia.petkova@gmail.com>
2017-06-04 17:17:40 +0300

    add 4-3-13 Descript Messages

---
+++ 04-DataTypeAndVariables/4-3-13-DecryptingMessages/4-3-13-DecryptingMessages.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_13_DecryptingMessages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int key = int.Parse(Console.ReadLine());
+
+            int numChars = int.Parse(Console.ReadLine());
+
+            string message = "";
+            
+            for (int i = 1; i <= numChars; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                int letterTemp = Convert.ToInt32(letter);
+
+                int letterFitted = letterTemp + key;
+
+                letter = Convert.ToChar(letterFitted);
+                message += letter;
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
