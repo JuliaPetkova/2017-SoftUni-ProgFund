6b10f9e7271c53e556b3d6af02eaaea9d9449d34
JuliaPetkova <julia.petkova@gmail.com>
2017-06-04 09:57:26 +0300

    add task 4-3-9 Make a word

---
+++ 04-DataTypeAndVariables/4-3-09-MakeWord/4-3-09-MakeWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_3_09_MakeWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string word = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                word = word + letter;
+            }
+            Console.WriteLine($"The word is: {word}");
+
+        }
+    }
+}
