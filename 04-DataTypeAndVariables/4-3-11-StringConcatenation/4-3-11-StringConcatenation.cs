e3dd36b2a83563ed7597e804fd2813a54c182f38
JuliaPetkova <julia.petkova@gmail.com>
2017-06-04 16:16:21 +0300

    add task 4-3-11 String Concatenation - TrimEnd(string)

--- 04-DataTypeAndVariables/4-3-11-StringConcatenation/4-3-11-StringConcatenation.cs
+++ 04-DataTypeAndVariables/4-3-11-StringConcatenation/4-3-11-StringConcatenation.cs
@@ -10,6 +10,36 @@ namespace _4_3_11_StringConcatenation
     {
         static void Main(string[] args)
         {
+            char delimeter = char.Parse(Console.ReadLine());
+            string evenOdd = Console.ReadLine().ToLower();
+            int stringNumbers = int.Parse(Console.ReadLine());
+
+            string strConcatenation = string.Empty;
+
+            for (int i = 1; i <= stringNumbers; i++)
+            {
+                string strName = Console.ReadLine();
+
+                switch (evenOdd)
+                {
+                    case "even":
+                        if (i % 2 == 0)
+                        {
+                            strConcatenation = strConcatenation + strName + delimeter;
+                        }
+                        break;
+                    case "odd":
+                        if (i % 2 != 0)
+                        {
+                            strConcatenation = strConcatenation + strName + delimeter;
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine($"{strConcatenation.TrimEnd(delimeter)}");
         }
     }
 }
