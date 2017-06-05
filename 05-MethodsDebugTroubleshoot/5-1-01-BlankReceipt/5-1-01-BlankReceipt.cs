943ab683ee753782ade7d51b187e0adee69b2036
JuliaPetkova <julia.petkova@gmail.com>
2017-06-05 23:54:37 +0300

    add task 1, topic 5 first Method

---
+++ 05-MethodsDebugTroubleshoot/5-1-01-BlankReceipt/5-1-01-BlankReceipt.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_1_01_BlankReceipt
+{
+    class Program
+    {
+        static void PritReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+
+        static void PrintReceiptFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("\u00A9 SoftUni");
+        }
+
+        static void PrintPeceiptBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void PrintReceipt()
+        {
+            PritReceiptHeader();
+            PrintPeceiptBody();
+            PrintReceiptFooter();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+        }
+    }
+}
