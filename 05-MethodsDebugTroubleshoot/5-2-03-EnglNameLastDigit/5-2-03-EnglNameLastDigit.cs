469cf83bbb77c805179141605b3a01ba3d0437ce
JuliaPetkova <julia.petkova@gmail.com>
2017-06-06 23:12:24 +0300

    rename directories

---
+++ 05-MethodsDebugTroubleshoot/5-2-03-EnglNameLastDigit/5-2-03-EnglNameLastDigit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_03_EnglNameLastDigit
+{
+    class Program
+    {
+
+        static void LastDigitName()
+        {
+            int num = int.Parse(Console.ReadLine());
+            num = num % 10;
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("one");
+                    break;
+
+                case 2:
+                    Console.WriteLine("two");
+                    break;
+
+                case 3:
+                    Console.WriteLine("three");
+                    break;
+
+                case 4:
+                    Console.WriteLine("four");
+                    break;
+
+                case 5:
+                    Console.WriteLine("five");
+                    break;
+
+                case 6:
+                    Console.WriteLine("six");
+                    break;
+
+                case 7:
+                    Console.WriteLine("seven");
+                    break;
+
+                case 8:
+                    Console.WriteLine("eight");
+                    break;
+
+                case 9:
+                    Console.WriteLine("nine");
+                    break;
+
+                default:
+                    Console.WriteLine("zеro");
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            LastDigitName();
+        }
+    }
+}
---
+++ 05-MethodsDebugTroubleshoot/5-2-04-NumOfReverseOrder/5-2-04-NumOfReverseOrder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_04_NumOfReverseOrder
+{
+    class Program
+    {
+        static void PrintNumberInReverseOrder()
+
+        {
+            double num = double.Parse(Console.ReadLine());
+                        
+            //int numDigits =num.L;
+            num = double.ToString().ToCharArray();
+            
+
+            for (int i = 1; i <= numDigits; i++)
+            {
+                num = num % 10;               
+
+                Console.Write(num);
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            //  PrintNumberInReverseOrder();
+        }
+    }
+}
