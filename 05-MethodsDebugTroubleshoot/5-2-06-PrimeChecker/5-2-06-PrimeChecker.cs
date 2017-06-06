d0e941e5f392d6bc97c1fb15e17a7fca1df79ea7
JuliaPetkova <julia.petkova@gmail.com>
2017-06-07 01:28:57 +0300

    add Prime Checker

---
+++ 05-MethodsDebugTroubleshoot/5-2-06-PrimeChecker/5-2-06-PrimeChecker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_2_06_PrimeChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long num = long.Parse(Console.ReadLine());
+
+            bool isPrime = IsPrime(num);
+            Console.WriteLine(isPrime);
+        }
+
+        private static bool IsPrime(long num)
+        {
+            bool isPrime = true;
+
+            if (num == 0 || num == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i <= Math.Sqrt(num); i++) // for (int i = 2; i < num; i++) too long
+            {
+                if (num % i == 0)
+                {
+                    isPrime = false;
+                }
+            }
+            return isPrime;
+        }
+    }
+}
