8dc4dda092943fd021ac73d8006df1e07715aae4
JuliaPetkova <julia.petkova@gmail.com>
2017-06-08 23:27:43 +0300

    add new tasks

---
+++ 06-Arrayas/6-1-06-ReverseAnArrayOfStrings/6-1-06-ReverseAnArrayOfStrings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_1_06_ReverseAnArrayOfStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ');
+
+            Array.Reverse(array);
+
+            foreach (string value in array)
+            {
+                Console.Write(value+' ');
+            }
+            Console.WriteLine();
+        }
+    }
+}
