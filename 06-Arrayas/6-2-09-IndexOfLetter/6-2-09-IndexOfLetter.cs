1750c15ea185a95a5924d2b15866ac554ef05e02
JuliaPetkova <julia.petkova@gmail.com>
2017-06-11 21:34:29 +0300

    add Index of letters

--- 06-Arrayas/6-2-09-IndexOfLetter/6-2-09-IndexOfLetter.cs
+++ 06-Arrayas/6-2-09-IndexOfLetter/6-2-09-IndexOfLetter.cs
@@ -10,17 +10,17 @@ namespace _6_2_09_IndexOfLetter
     {
         static void Main(string[] args)
         {
-            string word = Console.ReadLine();
+            string str = Console.ReadLine();
 
-            char[] arr = new char[26]; // 26 are the Engl alphabeta
+            char[] arr = new char[26]; // 26 are the Engl alphabet
 
             for (int i = 0; i < arr.Length; i++)
             {
                 arr[i] = (char)(97 + i);
             }
-            for (int i = 0; i < word.Length; i++)
+            for (int i = 0; i < str.Length; i++)
             {
-                Console.WriteLine(word[i] + " -> " + Array.IndexOf(arr, word[i]));
+                Console.WriteLine(str[i] + " -> " + Array.IndexOf(arr, str[i]));
             }
 
         }
