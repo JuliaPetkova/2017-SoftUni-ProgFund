a4df861032844b80c095524a55913ab6361a44bd
JuliaPetkova <julia.petkova@gmail.com>
2017-06-13 01:15:23 +0300

    add

---
+++ 06-Arrayas/6-3-01-ArrayStatistics-ver2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3_01_ArrayStatistics_ver2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int min = CalculateMinimalValue(arr);
+            int max = CalculateMaximalValue(arr);
+            long sum = CalculateSimOfElements(arr);
+            double average = (double)sum/arr.Length;
+
+            Console.WriteLine($"Min = {min}");
+            Console.WriteLine($"Max = {max}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {Math.Round(average,1)}");
+        }
+
+        private static long CalculateSimOfElements(int[] arr)
+        {
+            long sum = 0;
+            foreach (var item in arr)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+
+        private static int CalculateMaximalValue(int[] arr)
+        {
+            int max = int.MinValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+
+        private static int CalculateMinimalValue(int[] arr)
+        {
+            int min = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            return min;
+        }
+    }
+}
---
+++ 06-Arrayas/6-3-01-ArrayStatistics/6-3-01-ArrayStatistics.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3_01_ArrayStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int maxNum = int.MinValue;
+            int minNum = int.MaxValue;
+            int sum = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum = sum + arr[i];
+
+                if (minNum > arr[i])
+                {
+                    minNum = arr[i];
+                }
+                else
+                {
+                    minNum = minNum;
+                }
+
+                if (maxNum < arr[i])
+                {
+                    maxNum = arr[i];
+                }
+                else
+                {
+                    maxNum = maxNum;
+                }
+
+            }
+            double average = (double)sum / arr.Length;
+            Console.WriteLine($"Min = {minNum}");
+            Console.WriteLine($"Max = {maxNum}");
+            Console.WriteLine($"Sum = {sum}");
+            Console.WriteLine($"Average = {average:F1}");
+        }
+    }
+}
