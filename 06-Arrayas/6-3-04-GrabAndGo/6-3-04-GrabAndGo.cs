a37811f9cb7b036f0aa3da8157b4e1d5c3b0d633
JuliaPetkova <julia.petkova@gmail.com>
2017-06-15 00:33:07 +0300

    add new tasks

---
+++ 06-Arrayas/6-3-04-GrabAndGo/6-3-04-GrabAndGo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3_04_GrabAndGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int findNum = int.Parse(Console.ReadLine());
+            
+            long sum = 0;
+            int index;
+
+            bool isNumberFaund = false;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                if (arr[i] == findNum)
+                {
+                    index = i;
+                    isNumberFaund = true;
+                    for (int j = 0; j < index; j++)
+                    {
+                        sum += arr[j];
+                    }
+                    break;
+                }                 
+            }
+            if (isNumberFaund == false)
+            {
+                Console.WriteLine("No occurrences were found!");
+            }
+            else
+            {
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
---
+++ 06-Arrayas/6-3-05-PizzaIngredients/6-3-05-PizzaIngredients.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_3_05_PizzaIngredients
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+            int ingradientLenght = int.Parse(Console.ReadLine());
+
+            List<string> pizaProducts = new List<string>();
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var currentString = arr[i];
+
+                if (currentString.Length == ingradientLenght)
+                {
+                    count++;
+                    Console.WriteLine($"Adding {currentString}.");
+                    pizaProducts.Add(currentString);
+
+                    if (pizaProducts.Count == 10)
+                    {
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Made pizza with total of {count} ingredients.");
+            Console.WriteLine("The ingredients are: " + string.Join(", ", pizaProducts) + ".");
+        }
+    }
+}
