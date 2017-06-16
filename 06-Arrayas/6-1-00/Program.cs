04c3a499785ae59cee7c61ab75c27dc1de0642e0
JuliaPetkova <julia.petkova@gmail.com>
2017-06-16 20:25:38 +0300

    add

--- 06-Arrayas/6-1-00/Program.cs
+++ 06-Arrayas/6-1-00/Program.cs
@@ -4,58 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
-
-
 namespace _program
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] prices = Console.ReadLine().Split().Select(int.Parse).ToArray();
-            int priceJewels = prices[0];
-            int priceGold = prices[1];
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
-            long earnings = 0;
-            long expensеs = 0;
+            int min = MinValue(arr);
+            Console.WriteLine($"Min = {min}");
+ 
+        }
 
-            string str = Console.ReadLine();
-
-            while (str != "Jail Time")
+        static int MinValue(int[] arr)
+        {
+            foreach (var item in arr)
             {
-                string[] currentStr = str.Split(' ');
-
-                string priceEarns = currentStr[0];
-                long priceExps = long.Parse(currentStr[1]);
-
-                expensеs += priceExps;
-
-                foreach (char item in priceEarns)
-                {
-                    if (item == '%')
-                    {
-                        earnings += priceJewels;
-                    }
-                    if (item == '$')
-                    {
-                        earnings += priceGold;
-                    }
-                }
-                str = Console.ReadLine();
+                min
             }
-
-
-            long total = Math.Abs(earnings - expensеs);
-            if (earnings >= expensеs)
-            {
-                Console.WriteLine($"Heists will continue. Total earnings: {total}.");
-            }
-            else
-            {
-                Console.WriteLine($"Have to find another job. Lost: {total}.");
-            }
-
         }
     }
 
---
+++ 07-Lists/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Dictionary<string, double>();
+            students.Add("pesho", 5.33);
+            students.Add("vercheto", 2.55);
+            students.Add("Neda", 6.00);
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.Key + "-->" + item.Value);
+            }
+
+        }
+    }
+}
